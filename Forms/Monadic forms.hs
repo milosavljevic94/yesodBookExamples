3426b38d9506183a073dd74fef91faa478bc13b0
Nemanja <nemanjam2008@gmail.com>
2021-01-12 16:47:43 +0100

    Forms examples

---
+++ Forms/Monadic forms.hs
@@ -0,0 +1,59 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+import Control.Applicative
+import Data.Text (Text)
+import Yesod
+
+data App = App
+
+mkYesod "App" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod App
+
+instance RenderMessage App FormMessage where
+    renderMessage _ _ = defaultFormMessage
+
+data Person = Person{
+        name :: Text,
+        age :: Int
+    }
+    deriving Show
+
+personForm :: Html -> MForm Handler (FormResult Person, Widget)
+personForm extra = do
+    (nameRes, nameView) <- mreq textField "this is not used" Nothing
+    (ageRes, ageView) <- mreq intField "neither is this" Nothing
+    let personRes = Person <$> nameRes <*> ageRes
+    let widget = do
+        toWidget[lucius|
+            ##{fvId ageView} {
+                    width: 3em;
+                    }
+        |]
+        [whamlet|
+                #{extra}
+                <p>
+                    Hello, my name is #
+                    ^{fvInput nameView}
+                    \ and I am #
+                    ^{fvInput ageView}
+                    \ years old. #
+                    <input type=submit value="Introduce myself">
+            |]
+    return (personRes, widget)
+
+getHomeR :: Handler Html
+getHomeR = do
+    ((res, widget), enctype) <- runFormGet personForm
+    defaultLayout[whamlet|
+            <p>Result: #{show res}
+            <form enctype=#{enctype}>
+                ^{widget}
+        |]
+main :: IO()
+main = warp 3000 App
